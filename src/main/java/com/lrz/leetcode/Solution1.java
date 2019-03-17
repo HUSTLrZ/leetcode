f3b0f186ec42b7c0595f46f4749bce62d2cefb69
hustlrz <273573682@qq.com>
2019-03-17 21:04:52 +0800

    sudoku

--- src/main/java/com/lrz/leetcode/Solution1.java
+++ src/main/java/com/lrz/leetcode/Solution1.java
@@ -4,4 +4,52 @@ package com.lrz.leetcode;
  * 九宫格
  */
 public class Solution1 {
+    public void solveSudoku(char[][] board) {
+        if (board == null || board.length < 3) {
+            return;
+        }
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0;j < board.length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, c, i, j)) {
+                            board[i][j] = c;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, char c, int row, int col) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == c) {
+                return false;
+            }
+            if (board[row][i] == c) {
+                return false;
+            }
+        }
+
+        int x = (row / 3) * 3;
+        int y = (col / 3) * 3;
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                if (board[i][j] == c) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
